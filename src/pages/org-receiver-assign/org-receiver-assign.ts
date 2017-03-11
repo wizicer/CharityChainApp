ad7dd92956ba9c6344386e72a5d3cb46b5ed528b
Icer <liangshuangde@163.com>
2017-03-11 20:33:37 +0800

    add basic assign page

--- src/pages/org-receiver-assign/org-receiver-assign.ts
+++ src/pages/org-receiver-assign/org-receiver-assign.ts
@@ -1,22 +1,86 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
-/*
-  Generated class for the OrgReceiverAssign page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-org-receiver-assign',
   templateUrl: 'org-receiver-assign.html'
 })
 export class OrgReceiverAssignPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  @ViewChild('fileInput') fileInput;
+
+  isReadyToSave: boolean;
+
+  item: any;
+
+  form: FormGroup;
+
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController, formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      profilePic: [''],
+      name: ['', Validators.required],
+      about: ['']
+    });
+
+    // Watch the form for changes, and
+    this.form.valueChanges.subscribe((v) => {
+      this.isReadyToSave = this.form.valid;
+    });
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrgReceiverAssignPage');
+
+  }
+
+  getPicture() {
+    if (Camera['installed']()) {
+      Camera.getPicture({
+        targetWidth: 96,
+        targetHeight: 96
+      }).then((data) => {
+        this.form.patchValue({ 'profilePic': 'data:image/jpg;base64,' +  data });
+      }, (err) => {
+        alert('Unable to take photo');
+      })
+    } else {
+      this.fileInput.nativeElement.click();
+    }
+  }
+
+  processWebImage(event) {
+    let input = this.fileInput.nativeElement;
+
+    var reader = new FileReader();
+    reader.onload = (readerEvent) => {
+      input.parentNode.removeChild(input);
+
+      var imageData = (readerEvent.target as any).result;
+      this.form.patchValue({ 'profilePic': imageData });
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  getProfileImageStyle() {
+    return 'url(' + this.form.controls['profilePic'].value + ')'
+  }
+
+  /**
+   * The user cancelled, so we dismiss without sending data back.
+   */
+  cancel() {
+    this.viewCtrl.dismiss();
+  }
+
+  /**
+   * The user is done and wants to create the item, so return it
+   * back to the presenter.
+   */
+  done() {
+    if(!this.form.valid) { return; }
+    this.viewCtrl.dismiss(this.form.value);
   }
 
 }
